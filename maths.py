29df25c57f8144925d362aab4f741565e25ce46e
LiamTHCH <liam.thomaschollier@gmail.com>
2022-02-23 10:50:55 +0100

    maj TP2
    Signed-off-by: LiamTHCH <liam.thomaschollier@gmail.com>

--- maths.py
+++ maths.py
@@ -1,5 +1,4 @@
 from math import *
-
 class Point:
     def __init__(self,x,y):
         self.__x = x
@@ -30,4 +29,13 @@ class Point:
     def double_distance_Point(self,Point):
         return sqrt(((self.__x-Point.get_x())**2)+((self.__y-Point.get_y())**2))
 
-    
+class Cercle:
+    def __init__(self,position=(0,0),rayon=0):
+        self.__position = position
+        self.__rayon = rayon
+    def diametre(self):
+        return self.__rayon*2
+    def perimetre(self):
+        return 2*pi*self.__rayon
+    def surface(self):
+        return pi*(self.__rayon**2)
